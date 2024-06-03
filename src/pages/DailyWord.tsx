4828f121f4fb9db630536663a604740619e511b5
starlash7 <yang92sw@gmail.com>
2024-06-03 15:44:40 +0900

    first commit

---
+++ src/pages/DailyWord.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import { Flex } from "@chakra-ui/react";
+
+const DailyWord: FC = () => {
+  return <Flex>DailyWord</Flex>;
+};
+
+export default DailyWord;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import { Flex } from "@chakra-ui/react";
+
+const Home: FC = () => {
+  return <Flex>Home</Flex>;
+};
+
+export default Home;
