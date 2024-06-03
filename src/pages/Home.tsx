6021e7a4b70cea056282272a6a2ba9dffb64b802
starlash7 <yang92sw@gmail.com>
2024-06-03 16:27:05 +0900

    Home 완성

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,30 @@
-import { FC } from "react";
-import { Flex } from "@chakra-ui/react";
+import { Button, Flex, Text } from "@chakra-ui/react";
+import { FC, useState } from "react";
+import sampleData from "../assets/sampleData.json";
 
 const Home: FC = () => {
-  return <Flex>Home</Flex>;
+  const [wordsData, setWordsData] = useState<IWords[]>(sampleData);
+
+  return (
+    <Flex flexDir="column" maxW={768} mx="auto" minH="100vh">
+      <Text fontSize={48} fontWeight="bold" textAlign="center" mt={8}>
+        Word App
+      </Text>
+      <Flex flexDir="column" mt={8} gap={4} px={4}>
+        {wordsData.map((v) => (
+          <Button
+            key={v.day}
+            variant="outline"
+            colorScheme="pink"
+            justifyContent="start"
+            isTruncated={true}
+          >
+            <Text fontWeight="bold">Day {v.day}</Text>-{v.title}
+          </Button>
+        ))}
+      </Flex>
+    </Flex>
+  );
 };
 
 export default Home;
