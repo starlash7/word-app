30e1a8d29b5a3a726d66cf112fd4561c154d6114
starlash7 <yang92sw@gmail.com>
2024-06-04 13:19:30 +0900

    Another Daily word

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -16,11 +16,31 @@ const Home: FC = () => {
           <Button
             key={v.day}
             variant="outline"
-            colorScheme="pink"
+            colorScheme="green"
             justifyContent="start"
             isTruncated={true}
             onClick={() =>
               navigate(`/daily-word/${v.day}`, {
+                state: {
+                  wordData: v,
+                },
+              })
+            }
+          >
+            <Text fontWeight="bold">Day {v.day}</Text> - {v.title}
+          </Button>
+        ))}
+      </Flex>
+      <Flex flexDir="column" mt={16} gap={4} px={4}>
+        {sampleData.map((v: IWords) => (
+          <Button
+            key={v.day}
+            variant="outline"
+            colorScheme="pink"
+            justifyContent="start"
+            isTruncated={true}
+            onClick={() =>
+              navigate(`/another-daily-word/${v.day}`, {
                 state: {
                   wordData: v,
                 },
